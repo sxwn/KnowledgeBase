9fe7b9d59f67d7138e00fd40bd22262c33c56ed6
xiaowei <wdzaxiaowei@126.com>
2021-05-28 22:10:32 +0800

    apk瘦身

--- app/src/main/interview/ui/apk_optional.java
+++ app/src/main/interview/ui/apk_optional.java
@@ -58,4 +58,41 @@ apk瘦身: 代码质量
      启动界面：
     图：
     webp格式  --android
-    png  透明度 --->jpg
+    图片   ：透明度png --->jpg
+
+        Android 国际化:
+        // 只加载英文和中文
+        resConfigs 'en','zh'
+        defaultConfig {
+            applicationId "com.xiaowei.xiaobai"
+            minSdkVersion 19
+            targetSdkVersion 29
+            versionCode 1
+            versionName "1.0"
+            testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+            resConfigs 'en','zh'，'zh-rHK'
+        }
+
+     动态库瘦身：
+        so ---依赖库
+        jar
+        c/c++
+        如果你是定制机  中间件,需要和底层工程师沟通
+        如果你是手机 一般就放一个，arm64-v8a
+        ndk{
+            abiFilters 'arm64-v8a','x86'
+        }
+
+        APK拆分打包
+
+    移除无用资源:
+    1、androidstudio带了工具，Refactor-->Remove Unused resource。  需要对代码进行一些检查，然后再删。
+    2、Analyze--->Run inspection by name -> unused resource.  lint检查
+
+    非严苛模式
+    严苛模式---
+
+    资源混淆----
+
+    zip压缩
+    gradle插件
